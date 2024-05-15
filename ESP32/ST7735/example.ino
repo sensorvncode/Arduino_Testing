21685c4d0777806c9011c7ed1c10fb249a5c43c5
sensorvn.com <42823780+sensorvncom@users.noreply.github.com>
2024-05-15 18:08:28 +0700

    Update example.ino

--- ESP32/ST7735/example.ino
+++ ESP32/ST7735/example.ino
@@ -13,23 +13,28 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 void setupST7735() {
   tft.initR(INITR_144GREENTAB);
   tft.fillScreen(ST77XX_BLACK);
-  printLCD("ST7735 OK");
+  printLcd("ST7735 OK");
+  delay(200);
 }
-void testdrawtext(char *text, uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(0, 0);
+void printLcdAxis(int x, int y, char *text, uint16_t color) {
+  tft.setCursor(x, y);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.print(text);
 }
-void printLCD(char *text) {
-  testdrawtext(text, ST77XX_WHITE);
+void printLcd(char *text) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextWrap(true);
+  tft.print(text);
 }
 
 /* Main Function */
 void setup(void) {
   // TFT 1.44
   setupST7735();
+  printLcdAxis(0, 12, "Le Xuan Khanh", ST77XX_YELLOW);
 }
 
 void loop() {
