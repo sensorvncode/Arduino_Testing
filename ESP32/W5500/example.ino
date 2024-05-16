c9cbc24eb17e8d6446cc18bb6b8c52bded78f12c
sensorvn.com <42823780+sensorvncom@users.noreply.github.com>
2024-05-16 19:31:29 +0700

    Create example.ino

---
+++ ESP32/W5500/example.ino
@@ -0,0 +1,109 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+/* My Variables */
+//char server[] = "httpbin.org";  // name address for Google (using DNS)
+IPAddress server(54, 87, 148, 249);  // (no DNS)
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+// Set the static IP address to use if the DHCP fails to assign
+#define MYIPADDR 192, 168, 1, 28
+#define MYIPMASK 255, 255, 255, 0
+#define MYDNS 192, 168, 1, 1
+#define MYGW 192, 168, 1, 1
+EthernetClient client;
+// Variables to measure the speed
+unsigned long beginMicros, endMicros;
+unsigned long byteCount = 0;
+bool printWebData = true;  // set to false for better speed measurement
+
+/* Main Function */
+void setup() {
+  // Serial
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Begin Ethernet");
+  // W5500
+  Ethernet.init(5);
+  if (Ethernet.begin(mac)) {  // Dynamic IP setup
+    Serial.println("DHCP OK!");
+  } else {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1);  // do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    IPAddress ip(MYIPADDR);
+    IPAddress dns(MYDNS);
+    IPAddress gw(MYGW);
+    IPAddress sn(MYIPMASK);
+    Ethernet.begin(mac, ip, dns, gw, sn);
+    Serial.println("STATIC OK!");
+  }
+  delay(1000);
+  Serial.print("Local IP : ");
+  Serial.println(Ethernet.localIP());
+  Serial.print("Subnet Mask : ");
+  Serial.println(Ethernet.subnetMask());
+  Serial.print("Gateway IP : ");
+  Serial.println(Ethernet.gatewayIP());
+  Serial.print("DNS Server : ");
+  Serial.println(Ethernet.dnsServerIP());
+  Serial.println("Ethernet Successfully Initialized");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("Connected!");
+    // Make a HTTP request:
+    client.println("GET /get HTTP/1.1");
+    client.println("Host: httpbin.org");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    // if you didn't get a connection to the server:
+    Serial.println("connection failed");
+  }
+  beginMicros = micros();
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  int len = client.available();
+  if (len > 0) {
+    byte buffer[80];
+    if (len > 80) len = 80;
+    client.read(buffer, len);
+    if (printWebData) {
+      Serial.write(buffer, len);  // show in the serial monitor (slows some boards)
+    }
+    byteCount = byteCount + len;
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    endMicros = micros();
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+    Serial.print("Received ");
+    Serial.print(byteCount);
+    Serial.print(" bytes in ");
+    float seconds = (float)(endMicros - beginMicros) / 1000000.0;
+    Serial.print(seconds, 4);
+    float rate = (float)byteCount / seconds / 1000.0;
+    Serial.print(", rate = ");
+    Serial.print(rate);
+    Serial.print(" kbytes/second");
+    Serial.println();
+
+    // do nothing forevermore:
+    while (true) {
+      delay(1);
+    }
+  }
+}
