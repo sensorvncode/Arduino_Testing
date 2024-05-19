f5594da7b3ac0088c8b4097bc2958d1574032af4
sensorvn.com <42823780+sensorvncom@users.noreply.github.com>
2024-05-19 12:14:38 +0700

    Update example.ino

--- ESP32/LCD1602/example.ino
+++ ESP32/LCD1602/example.ino
@@ -1,1 +1,27 @@
-s
+/*
+ * Connect
+ *      Arduino       I2C_LCD
+ *        5V            VCC
+ *        GND           GND
+ *        A4            SDA
+ *        A5            SCL
+ */
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+void setup()
+{
+  // Initialize the lcd 
+  lcd.init();                     
+  // Print a message to the LCD.
+  lcd.backlight();
+  lcd.setCursor(4,0);
+  lcd.print("sensorvn.com");
+  lcd.setCursor(2,1);
+  lcd.print("Hello world!");
+}
+
+void loop()
+{
+}
