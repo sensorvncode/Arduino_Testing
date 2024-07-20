26fa8325a880da49d30d953df83e3afe79e941a3
sensorvn.com <42823780+sensorvn-com@users.noreply.github.com>
2024-07-20 12:12:56 +0700

    Update RX_Single.ino

--- ESP32/UART_2_Arduino/RX_Single.ino
+++ ESP32/UART_2_Arduino/RX_Single.ino
@@ -10,7 +10,6 @@ void loop() {
     incomingData = Serial.readStringUntil('\n');  // Đọc đến khi gặp ký tự xuống dòng '\n'
     // In ra dữ liệu chỉ khi nhận được dữ liệu mới
     if (incomingData != "") {
-      Serial.print("I received: ");
       Serial.println(incomingData);
       incomingData = "";                // Đặt lại chuỗi nhận được để chuẩn bị nhận dữ liệu mới
       delay(10);                        // Đợi một chút để xử lý hoàn tất việc gửi và nhận dữ liệu
